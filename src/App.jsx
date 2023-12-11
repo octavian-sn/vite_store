16dc25bc7f67cf2abd949cc889bd314ae7837d87
Octavian Sulic (PDP) <octavian.sulic@IDGROUP.local>
2023-12-11 12:45:21 +0200

    Add footer.

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,23 @@ import Home from './pages/Home';
 import Details from './pages/Details';
 import Cart from './pages/Cart';
 import Admin from './pages/Admin';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <>
-      <Routes>
+    <div className='flex flex-col min-h-screen'>
+      <Navbar/>
+      <div className='flex-grow'>
+      <Routes className>
         <Route path='/'element={<Home/>}/>
         <Route path='/details' element={<Details/>}/>
         <Route path='/cart' element={<Cart/>}/>
 				<Route path='/admin' element={<Admin />} />
       </Routes>
-    </>
+      </div>
+      <Footer/>
+    </div>
   );
 };
 
