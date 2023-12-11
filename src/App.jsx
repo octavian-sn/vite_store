05282a849f7ff821ac374a8177d252a1aa58afae
Octavian Sulic (PDP) <octavian.sulic@IDGROUP.local>
2023-12-11 17:30:09 +0200

    Add flowbite dependency.

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <div className='flex flex-col min-h-screen'>
       <Navbar/>
       <div className='flex-grow'>
-      <Routes className>
+      <Routes >
         <Route path='/'element={<Home/>}/>
         <Route path='/details' element={<Details/>}/>
         <Route path='/cart' element={<Cart/>}/>
